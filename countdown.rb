5a2c73c594ddc47acb967a5fc3a08a62a8eb9927
Scott Ungchusri <animoogle504@gmail.com>
2018-06-25 23:03:02 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,15 @@
 #write your code here
 
 def countdown(count)
-  
+ while count > 0 
+  puts "#{number} SECOND(S)!"
+end
+  puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(count)
-  
+  while count > 0 
+  puts "#{number} SECOND(S)!"
+end
+  puts "HAPPY NEW YEAR!"
 end
